fbb89e3e236a9913b9e47131920a59654b80108e
BALAJI POTHULA <balan.pothula@gmail.com>
2019-05-06 18:40:15 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,14 +34,18 @@ apt update
 apt -y install docker-ce docker-ce-cli containerd.io
 
 # extracting "webapp" tar ball and removing.
-tar xzf $HOME/tomcat-8.5.40/webapp.tar.gz -C $HOME/tomcat-8.5.40/ && rm $HOME/tomcat-8.5.40/webapp.tar.gz
+tar xzf $HOME/tomcat-8.5.40-redis-4.0.11/webapp.tar.gz -C $HOME/tomcat-8.5.40-redis-4.0.11/ && rm $HOME/tomcat-8.5.40-redis-4.0.11/webapp.tar.gz
 
 # pulling tomcat:8.5.40 image from docker hub.
 docker pull balajipothula/tomcat:8.5.40
 
 # running docker container with name(--name) "tomcat" as daemon(-d),
 # stdin(-i) with volume(-v) "webapp" on port(-p) "8080".
-docker run --name tomcat -d -i -p 8080:8080 --privileged -v $HOME/tomcat-8.5.40/webapp:/webapp balajipothula/tomcat:8.5.40 sh
+docker run --name tomcat_redis -d -i -p 8080:8080 --privileged -v $HOME/tomcat-8.5.40-redis-4.0.11/webapp:/webapp balajipothula/tomcat:8.5.40 sh
 
 # executing docker container by name with stdin(-i), starting tomcat server.
-docker exec -i tomcat /webapp/tomcat/bin/startup.sh
+docker exec -i tomcat_redis /webapp/tomcat/bin/startup.sh
+
+# executing docker container by name with stdin(-i), starting redis server.
+docker exec -i tomcat_redis redis-server /webapp/redis/conf/redis.conf
+
